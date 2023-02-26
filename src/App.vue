3ef58b992dc07439599a7061acbe60d5c9b74134
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-26 20:11:19 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="bg-blue">
+      <QRcode/>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import QRcode from '@/components/QRcode.vue'
+</script>
---
+++ src/components/QRcode.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+      <canvas id="qr" class="mx-auto"></canvas>
+  </v-layout>
+  <v-layout>
+    <p class="mx-auto text-white mt-2">QR code for this App</p>
+  </v-layout>
+</template>
+
+<script>
+import QRCode from 'qrcode';
+export default {
+  mounted: function(){
+    var currentUrl = window.location.origin;
+    console.log(currentUrl);
+    QRCode.toCanvas(document.getElementById('qr'),
+      currentUrl, { toSJISFunc: QRCode.toSJIS }, function (error) {
+      if (error) console.error(error)
+      console.log('success!')
+    })
+  }
+}
+</script>
+
+<style>
+  *  {
+    font-size: 16px;
+  }
+</style>
