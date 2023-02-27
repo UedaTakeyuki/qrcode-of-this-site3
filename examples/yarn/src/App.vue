9650326150845c00325ebabedb44ccccc5556c0c
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-27 20:44:49 +0900

    example

---
+++ examples/yarn/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="bg-blue">
+        <QRcode />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import QRcode from 'qrcode-of-this-site3'
+</script>
